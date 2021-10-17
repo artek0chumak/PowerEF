bf0158f900676bf3966acaca8a84ea75447e4ba6
artek0chumak <artek.chumak@gmail.com>
2021-10-18 00:06:54 +0300

    Change models

--- main_cifar.py
+++ main_cifar.py
@@ -6,8 +6,8 @@ import wandb
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
-from torchvision.models import resnet18
 
+from model import resnet18
 from optimizer.sgd import ApproxSGD
 from optimizer.rank_ef import RankEF
 from optimizer.ef21 import EF21, EF21Plus
@@ -16,8 +16,8 @@ from optimizer.ef21 import EF21, EF21Plus
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--optimizer", choices=["sgd", "power_sgd", "power_ef", "power_ef_plus"], default="sgd")
-    parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--num_epoches", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=1024)
+    parser.add_argument("--num_epoches", type=int, default=100)
     parser.add_argument("--momentum", type=float, default=0.99)
     parser.add_argument("--weight-decay", type=float, default=1e-4)
     parser.add_argument("--lr", type=float, default=1e-1)
@@ -38,20 +38,35 @@ def weight_decay(model):
 def main(args):
     wandb.init(project='power_ef', entity='artek-chumak', config=args, mode="online")
 
-    transform = torchvision.transforms.Compose(
+    train_transform = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.RandomCrop(32, padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
+
+    valid_transform = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
         ]
     )
 
-    train_data = CIFAR10(args.cifar_root, download=True, transform=transform)
+    train_data = CIFAR10(args.cifar_root, download=True, transform=train_transform)
     train_dataloader = DataLoader(
-        train_data, batch_size=args.batch_size, sampler=torch.utils.data.RandomSampler(train_data)
+        train_data,
+        batch_size=args.batch_size,
+        sampler=torch.utils.data.RandomSampler(train_data),
+        num_workers=4
     )
-    valid_data = CIFAR10(args.cifar_root, train=False, download=True, transform=transform)
+    valid_data = CIFAR10(args.cifar_root, train=False, download=True, transform=valid_transform)
     valid_dataloader = DataLoader(
-        valid_data, batch_size=args.batch_size, shuffle=True
+        valid_data,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=4
     )
 
     torch.random.manual_seed(args.seed)
@@ -93,7 +108,7 @@ def main(args):
             for batch, target in tqdm(valid_dataloader):
                 if torch.cuda.is_available():
                     batch = batch.to("cuda")
-                pred = model(batch)[:, :10]
+                pred = model(batch)
                 if torch.cuda.is_available():
                     pred = pred.to("cpu")
                 pred = torch.argmax(pred, -1)
@@ -110,7 +125,7 @@ def main(args):
             if torch.cuda.is_available():
                 batch = batch.to("cuda")
                 target = target.to("cuda")
-            pred = model(batch)[:, :10]
+            pred = model(batch)
             loss = loss_fn(pred, target)
             if args.weight_decay > 0:
                 loss = loss + weight_decay(model) * args.weight_decay
